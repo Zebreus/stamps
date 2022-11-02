d92abf3b39bc7646f3151e0262c74ad8df239e16
Zebreus <lennarteichhorn@googlemail.com>
2022-11-02 23:09:34 +0100

    Add github ribbon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { GithubRibbon } from "components/GithubRibbon"
 import type { AppProps } from "next/app"
 // eslint-disable-next-line import/no-unassigned-import
 import "styles/global.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GithubRibbon />
+      <Component {...pageProps} />
+    </>
+  )
 }
